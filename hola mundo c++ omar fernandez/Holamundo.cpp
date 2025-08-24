d15cfd30599e8ac1a5245f38749f432e6402c99a
Omar-Fernandez-Quintanilla <omar.fernandez@ucb.edu.bo>
2025-08-24 13:50:42 -0400

    Practica-Hola mundo Omar fernandez Quintanilla

---
+++ hola mundo c++ omar fernandez/Holamundo.cpp
@@ -0,0 +1,20 @@
+// Materia: Programación 1, Paralelo 4
+// Autor: Omar Alejandro Fernández Quintanilla
+// Carnet: 9127397 L.P.
+// Carrera del estudiante: Ingeniería de Sistemas
+// Fecha creación: 24/08/2025
+// Número de ejercicio:HolaMundo
+//-----------------------------------------------------------------------
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 65001");
+    system("cls");
+    cout << "¡Hola, Mundo!" << endl;
+    cout << "Este es un programa de ejemplo en C++." << endl;
+    cout << "Usando la versión del estándar C++ 17." << endl;
+    return 0;
+}
